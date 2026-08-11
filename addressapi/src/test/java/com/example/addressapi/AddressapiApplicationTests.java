3df257c7cae42bb7439ec262df9ad3b44cb070c3
PJHREDDY108 <Pannuru.JagadeeswaraReddy@cognizant.com>
2026-08-11 12:46:10 +0530

    Configure H2 database for AddressapiApplicationTests
    Added test property source configuration for H2 database.

--- addressapi/src/test/java/com/example/addressapi/AddressapiApplicationTests.java
+++ addressapi/src/test/java/com/example/addressapi/AddressapiApplicationTests.java
@@ -2,16 +2,24 @@ package com.example.addressapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb3;DB_CLOSE_DELAY=-1",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
+})
 class AddressapiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-	@Test
-	void main() {
-		AddressapiApplication.main(new String[]{});
-	}
+    @Test
+    void contextLoads() {
+    }
+    @Test
+    void main() {
+       AddressapiApplication.main(new String[]{});
+    }
 
 }
